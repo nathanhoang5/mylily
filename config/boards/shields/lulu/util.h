882bfe876352e98974ca7513886367827cf55f02
Nathan Hoang <nathanhoang5@gmail.com>
2023-03-11 18:49:12 -0600

    hellladf

---
+++ config/boards/shields/lulu/util.h
@@ -0,0 +1,13 @@
+// Copyright 2022 jack (@waffle87)
+// SPDX-License-Identifier: MIT
+
+#define COMBO(name, keypress, keypos, layer) \
+combo_##name { \
+   layers = <layer>; \
+   timeout-ms = <50>; \
+   bindings = <keypress>; \
+   key-positions = <keypos>; \
+};
+
+#define HRML(k1,k2,k3,k4) &hm LALT k1 &hm LGUI k2 &hm LCTRL k3 &hm LSHFT k4
+#define HRMR(k1,k2,k3,k4) &hm RSHFT k1 &hm RCTRL k2 &hm RGUI k3 &hm RALT k4
